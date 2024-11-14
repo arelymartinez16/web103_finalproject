175f54b554902145808fd8590f82a224b126c84d
arelymartinez16 <arelymartinez741@gmail.com>
2024-11-14 18:46:13 -0500

    Changed styling of the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,12 +3,16 @@ const Login = (props) => {
     const AUTH_URL = `${props.api_url}/auth/github`
      
     return (
-        <div className="Login">
-            <h1>RoomMateLink 🏠</h1>
-            <center><a href={AUTH_URL}>
-                <button className="headerBtn"> 🔒 Login via Github </button>
-            </a></center>
-        </div>  
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+                <h1 className="text-3xl font-bold mb-4">RoomMateLink 🏠</h1>
+                <a href={AUTH_URL}>
+                <button className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
+                    🔒 Login via Github
+                </button>
+                </a>
+            </div>
+        </div>
     )
 }
 
