74bada226106a52c55ba10c3efc447e95ab5cf99
arelymartinez16 <arelymartinez741@gmail.com>
2024-11-14 19:04:28 -0500

    Defined url for profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 
-const Header = () => {
+const Header = ({ user_id }) => {
     return (
         <div className="header">
             <h1>RoomMateLink</h1>
             <Link to="/"><button className="headerBtn">Explore Rooms</button></Link>
             <Link to="/room/new"><button className="headerBtn"> + Add Room </button></Link>
+            <Link to={`/profile/${user_id}`}><button className="headerBtn">Profile</button></Link>
             <Link to="/login"><button className="headerBtn">Log Out</button></Link>
         </div>
     )
