8389c13f1c8e7c69bf93f19a477be74ae7e1ea72
arelymartinez16 <arelymartinez741@gmail.com>
2024-11-20 23:21:33 -0500

    Port

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,7 @@ const config = {
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     host: process.env.PGHOST,
-    port: process.env.PGPORT,
+    port: process.env.PGPORT || 5432,
     database: process.env.PGDATABASE
 }
 
